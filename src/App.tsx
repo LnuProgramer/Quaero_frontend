017d8caa3cabef0876f690bdbf8efd14c8f241d0
Denys <overfaken@gmail.com>
2024-11-16 21:49:04 +0200

    Start working on ProfileEmployee.tsx. Add box-shadow to Button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Security from './components/Security/Security';
 import Header from './components/Header/Header';
+import ProfileEmployee from "./components/Profiles/ProfileEmployee";
+import  "./App.scss"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Security />
+        {/*<Security />*/}
+        <ProfileEmployee />
     </div>
   );
 }
--- src/components/Profiles/ProfileEmployee.tsx
+++ src/components/Profiles/ProfileEmployee.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import Button from "../../reusableComponents/button/Button";
 import "./ProfileEmployee.scss"
+import Text from "../../reusableComponents/text/Text";
 
 function ProfileEmployee() {
     return (
-        <div className="profile">
-            <Button fontSize={20} buttonText={"Встановити контакт"} className={"profile-buttons"}/>
-            <Button fontSize={20} buttonText={"Встановити контакт"} buttonColor="primary" className={"profile-buttons"}/>
+        <div id="profile-employee">
+            <div className="bg-circle" id="left-middle-bg-circle"></div>
+            <div className="bg-circle" id="right-bottom-bg-circle"></div>
+            <div id="profile-employee-wrapper">
+                <div id="profile-employee-content-wrapper">
+                    <div id="photo-wrapper">
+                        <img id="profile-photo" src="/images/profile/UserDefaultPhoto.svg" alt="profile-photo"></img>
+                    </div>
+                    <div id="profile-employee-main-content-wrapper">
+                        <div id="profile-employee-left-content-wrapper">
+                            <div id="profile-employee-user-info-wrapper">
+                                <Text fontSize={30} as="h1">Ім’я Прізвище</Text>
+                                <Text fontSize={25} as="h2">Посада</Text>
+                                <Text fontSize={25} as="h2">Країна, місто</Text>
+                            </div>
+                            <div id="profile-employee-other-info-wrapper">
+                                <div className="profile-employee-block"></div>
+                                <div className="profile-employee-block"></div>
+                            </div>
+                        </div>
+                        <div id="profile-employee-right-content-wrapper">
+                            <div id="profile-employee-buttons-wrapper"></div>
+                            <div id="profile-employee-about-wrapper"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
