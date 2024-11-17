b49fa1d4a2480c9d5b212167413c14ac1d7a4b60
Denys <overfaken@gmail.com>
2024-11-17 21:16:42 +0200

    Finish working on ProfileEmployee.tsx. Fixed bugs

--- src/components/Profiles/ProfileEmployee.tsx
+++ src/components/Profiles/ProfileEmployee.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Button from "../../reusableComponents/button/Button";
 import "./ProfileEmployee.scss"
 import Text from "../../reusableComponents/text/Text";
+import "../../utils/i18n"
+import {useTranslation} from "react-i18next";
 
 function ProfileEmployee() {
+    const {t, i18n} = useTranslation();
+
     return (
         <div id="profile-employee">
             <div className="bg-circle" id="left-middle-bg-circle"></div>
@@ -21,13 +25,25 @@ function ProfileEmployee() {
                                 <Text fontSize={25} as="h2">Країна, місто</Text>
                             </div>
                             <div id="profile-employee-other-info-wrapper">
-                                <div className="profile-employee-block"></div>
-                                <div className="profile-employee-block"></div>
+                                <div className="profile-employee-block">
+                                    <Text fontSize={20} as="h2">{t("profileEmployee.myResume")}</Text>
+                                    <Text fontSize={20} as="a">{t("profileEmployee.downloadHere")}</Text>
+                                </div>
+                                <div className="profile-employee-block">
+                                    <Text fontSize={20} as="h2" id="additional-information-text">{t("profileEmployee.additionalInformation")}</Text>
+                                    <Text fontSize={20} as="p">Some Info</Text>
+                                </div>
                             </div>
                         </div>
                         <div id="profile-employee-right-content-wrapper">
-                            <div id="profile-employee-buttons-wrapper"></div>
-                            <div id="profile-employee-about-wrapper"></div>
+                            <div id="profile-employee-buttons-wrapper">
+                                <Button fontSize={20} fontWeight={500} buttonText={t("profileEmployee.establishContact")} className="profile-employee-buttons"/>
+                                <Button fontSize={20} fontWeight={500} buttonColor="primary" buttonText={t("profileEmployee.WriteMessage")} className="profile-employee-buttons"/>
+                            </div>
+                            <div id="profile-employee-about-wrapper" className="profile-employee-block">
+                                <Text fontSize={20} as="h2">{t("profileEmployee.AboutMe")}</Text>
+                                <Text fontSize={20} as="p">Some Text About Me </Text>
+                            </div>
                         </div>
                     </div>
                 </div>
